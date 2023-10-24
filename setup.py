90c8a7700f5477fb73e682f2e83fcf46b788fb63
jake-is-ESD-protected <jt@tschavoll.at>
2023-10-24 11:01:13 +0200

    name change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='schematic-line-tracing',
+    name='schematicLT',
     version='0.0.1',
     author='Konya, Tschavoll',
     author_email='your@email.com',
