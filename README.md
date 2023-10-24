7f69c68f3cb0bd0bbbc03f7847d91e8889627dfd
jake-is-ESD-protected <jt@tschavoll.at>
2023-10-24 08:49:24 +0200

    module boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # schematic-line-tracing
+
+## About
+Small python module for analytic line tracing of electrical schematics.
+
+## Setup
+```
+git clone https://github.com/jake-is-ESD-protected/schematic-line-tracing
+python3 -m venv .env
+source ./.env/bin/activate      # for Linux
+./.env/Scripts/activate         # for Windows
+pip install -r requirements.txt
+pip install -e .
+```
+
+## Structure
+Put all source code under `schematic-line-tracing` into a python file that encapsulates related functions and classes. Run tests in `tests`. Add all needed external modules to `requirements.txt`. This repo follows the standard python module structure.
+
+## Examples
+
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='schematic-line-tracing',
+    version='0.0.1',
+    author='Konya, Tschavoll',
+    author_email='your@email.com',
+    description='Assortment of line tracing tools for analytic schematic recognition',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+    ],
+    classifiers=[
+        'Development Status :: not released yet',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    url='https://github.com/jake-is-ESD-protected/schematic-line-tracing',
+    license='MIT',
+    keywords='schematic, image recognition',
+)
