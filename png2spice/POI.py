47a0ecf3da4e0b06d4429010b0132da3b91b75ee
KristofKonya <kristofvonkonya@hotmail.de>
2024-01-08 21:05:58 +0100

    moved dependencys into png2spice folder and adjusted paths

---
+++ png2spice/POI.py
@@ -0,0 +1,70 @@
+import numpy as np
+from enum import Enum
+
+class POITypes(Enum):
+   Resistor = 0
+   Capacitor = 1
+   Inductor = 2
+   Diode = 3
+   Corner = 4
+   Junction = 5
+   Cross = 6
+   GND = 7
+
+   @classmethod
+   def get_index(cls, type):
+      return list(cls).index(type)
+
+class POI:
+   def __init__(self):
+      self.type = None
+      self.position = None
+      self.rotation = 90
+      self.terminalA = None
+      self.terminalB = None
+      self.terminalC = None
+      self.terminalD = None
+      self.terminalALine = None
+      self.terminalBLine = None
+      self.terminalCLine = None
+      self.terminalDLine = None
+      self.value = None
+      self.text = None
+      self.marker = False
+      self.name = None
+
+   def __init__(self, val, pos, typ, name):
+      self.type = typ
+      self.position = pos
+      self.rotation = 0
+      self.terminalA = None
+      self.terminalB = None
+      self.terminalC = None
+      self.terminalD = None
+      self.terminalALine = None
+      self.terminalBLine = None
+      self.terminalCLine = None
+      self.terminalDLine = None
+      self.value = val
+      self.text = None
+      self.marker = False
+      self.name = name
+      self.confidence = 0
+
+   def printType(self):
+      print("My type is " + str(self.type))
+
+   def getPosition(self):
+      print("My position is " + str(self.position))
+   
+   def printInfo(self):
+      if(self != None):
+         print("Hi I am: " + str(self.value) + " and I am a " + str(self.type) + " @ " + str(self.position) + " " + str(self.name) + " R:" + str(self.rotation))
+
+
+def isValidPOI(classList):
+   return np.amax(classList) > 0.96
+
+def pred2Type(POI):
+   s = np.argmax(POI)
+   return POITypes(s)
