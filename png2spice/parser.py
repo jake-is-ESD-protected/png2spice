a561421cbce350a174727b0c0c7961f56d162346
jake-is-ESD-protected <jt@tschavoll.at>
2024-01-16 22:03:55 +0100

    parser WIP

---
+++ png2spice/parser.py
@@ -0,0 +1,40 @@
+from POI import POI, POITypes
+from typing import List
+
+# TODO: Anything WIRE-related
+
+class CParser():
+    def __init__(self, graph: List[POI]) -> None:
+        self.graph = graph
+        self.header = "SHEET 1 1000 1000"
+        self.part_aliases = dict({
+            f"{POITypes.Resistor}": "res",
+            f"{POITypes.Capacitor}": "cap",
+            f"{POITypes.Inductor}": "ind",
+            f"{POITypes.Diode}": "diode",
+            f"{POITypes.Corner}": None,
+            f"{POITypes.Junction}": None,
+            f"{POITypes.Cross}": None,
+            f"{POITypes.GND}": "0",            
+        })
+    
+    def Poi2Str(self, poi: POI) -> str:
+        posx = str(poi.position[0])
+        posy = str(poi.position[1])
+        rot = str(poi.rotation)
+        if poi.type <= POITypes.Diode:
+            return f"SYMBOL {self.part_aliases[f'{poi.type}']} {posx} {posy} R{rot}"
+        if poi.type == POITypes.GND:
+            return f"FLAG {posx} {posy} {self.part_aliases[f'{poi.type}']}"
+
+
+    def Graph2Asc(self, save_path: str="./output.asc", graph: List[POI]=None):
+        if graph:
+            self.graph = graph
+        with open(save_path) as f:
+            f.write(self.header + "\n")
+            for poi in self.graph:
+                f.write(self.Poi2Str(poi) + "\n")
+            
+        
+        
