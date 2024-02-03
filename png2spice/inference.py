28eb9d10c10a9bd50f5dc3fc3ab3331bcb92afee
jake-is-ESD-protected <jt@tschavoll.at>
2024-02-03 22:49:59 +0100

    inference as .py file with dict output

--- png2spice/inference.py
+++ png2spice/inference.py
@@ -1,6 +1,9 @@
 """
 This submodule of **png2spice** deals with inferencing SPICEnet and
-integrating its results into the entire process.
+integrating its results into the entire process. Application access
+to SPICEnet is managed via a virtual representation as its own 
+class. This class is not part of the standalone SPICEnet project/repo
+and only exists within the context of **png2spice**.
 """
 
 from keras.applications.vgg16 import preprocess_input
@@ -10,13 +13,24 @@ import numpy as np
 from parameters import P2SParameters
 from os.path import join
 import os
-from pytesseract import pytesseract
 import re
 from PIL import Image
 import json
 
+
 class CSPICEnet:
     def __init__(self, path: str) -> None:
+        """
+        BRIEF
+        -----
+        Abstraction object for the SPICEnet neural network.
+
+        PARAMETERS
+        ----------
+        `path`:
+            `str`. Path to the saved SPICEnet model in `.h5` format.
+
+        """
         self.model = load_model(join(path, "SPICEnet.h5"))
         self.imgResize = 224
         self.dataGenerator = ImageDataGenerator(
@@ -25,7 +39,24 @@ class CSPICEnet:
         with open(join(path, "CLASSLIST.json")) as f:
             self.classlist = json.load(f)["class_list"]
     
+
     def __predictRaw(self, path: str) -> np.ndarray:
+        """
+        BRIEF
+        -----
+        Get the raw output of the SPICEnet inference.
+        
+        PARAMETERS
+        ----------
+        `path`:
+            `str`. Path to the folder containing the data.
+            Since `keras` is used, the folder above the folder(s)
+            containing the data is expected.
+
+        RETURNS
+        -------
+        `np.ndarray`. Always of shape `(nPOIs, nClasses)`.
+        """
         pred = self.model.predict(
             ImageDataGenerator(
                 preprocessing_function=preprocess_input,
@@ -36,7 +67,30 @@ class CSPICEnet:
         )
         return pred
     
+
     def predict(self, path: str, show: bool=False):
+        """
+        BRIEF
+        -----
+        Get the predictions from the inference of SPICEnet.
+        
+        PARAMETERS
+        ----------
+        `path`:
+            `str`. Path to the folder containing the data.
+            Since `keras` is used, the folder above the folder(s)
+            containing the data is expected.
+        `show`:
+            `bool`. Show a plot listing all detected POIs and their
+            classifications. The used plotting backend has to be configured 
+            outside this function.
+        
+        RETURNS
+        -------
+        `dict`. Dict of dicts. Upper dict contains the labels of the POIs and
+        their sub-dicts. These consist of the component class names and their
+        probability per class.
+        """
         preds = self.__predictRaw(path)
         imageDisplayGenerator = self.dataGenerator.flow_from_directory(
             path,
@@ -47,20 +101,28 @@ class CSPICEnet:
 
         batch_images = imageDisplayGenerator.next()[0]
 
-        fig, axs = plt.subplots(nrows=int(imageDisplayGenerator.n/4), ncols=4, figsize=(20, 15)) # define your figure and axes
-        ind = 0
-        for ax1 in axs:
-            for ax2 in ax1:
-                ax2.imshow(batch_images[ind])
-                ax2.title.set_text(str(fileLabels[ind]) + " " + getType(preds[ind]) + "   R: %.2f C: %.2f I: %.2f D: %.2f \n Co: %.2f J: %.2f Cr: %.2f GND: %.2f" % (preds[ind][0],preds[ind][1],preds[ind][2],preds[ind][3],preds[ind][4],preds[ind][5],preds[ind][6],preds[ind][7]))
-                ind += 1
+        predDict = dict()
+        for pred, label in zip(preds, fileLabels):
+            classDict = dict()
+            for classVal, className in zip(pred, self.classlist):
+                classDict[className] = classVal
+            predDict[label] = classDict
 
-        fig.suptitle('Component Classifaction')
-        plt.tight_layout()
-        plt.show()
+        if show:
+            fig, axs = plt.subplots(nrows=int(imageDisplayGenerator.n/4), ncols=4, figsize=(40, 20))
+            ind = 0
+            for ax1 in axs:
+                for ax2 in ax1:
+                    ax2.imshow(batch_images[ind])
+                    partsFormat = " ".join([part[:2] + "%.2f\n" % preds[ind][i] for i, part in enumerate(self.classlist)])
+                    ax2.set_title(partsFormat, fontsize=10)
+                    ind += 1
 
-
-
+            fig.suptitle('Component Classifaction')
+            plt.tight_layout()
+            plt.show()
+        
+        return predDict
     
 
 if __name__ == "__main__":
@@ -69,5 +131,5 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
     SPICEnet = CSPICEnet("../SPICEnet")
-    print(SPICEnet.classlist)
+    p = SPICEnet.predict(join(P2SParameters.partSnapshotDir, ".."), show=True)
     # print(SPICEnet.predictRaw(join(P2SParameters.partSnapshotDir, "..")))
