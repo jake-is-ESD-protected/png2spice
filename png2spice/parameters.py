33b9c63e3aa5e84c57ec5ad28894c85ac084d421
jake-is-ESD-protected <jt@tschavoll.at>
2024-02-03 17:53:18 +0100

    introduce parameter submodule

---
+++ png2spice/parameters.py
@@ -0,0 +1,17 @@
+from os.path import join
+
+class CP2SParameters:
+    def __init__(self) -> None:
+        self.contrastThreshold      = 170
+        self.imagePadding           = 70
+        self.cannyThreshold         = 200
+        self.pointDistance          = 50
+        self.imageSliceSize         = 150   # The size of the subimages of POI analysis
+        self.HLThreshold            = 6     # Hough Lines Transform threshold
+        self.HLMinLineLength        = 48    # Hough Lines Transform minLineLength
+        self.HLmaxLineGap           = 2     # Hough Lines Transform maxLineGap
+        self.HoughIterations        = 10    # Amount of iterations
+        self.HoughThresholdWiggle   = 15
+        self.partSnapshotDir        = join(".temp", "output", "snapshots")
+
+P2SParameters = CP2SParameters()
