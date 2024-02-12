0fbf624893a76ffa8b682612d56e5ec10d3173b8
KristofKonya <kristofvonkonya@hotmail.de>
2024-02-12 11:44:40 +0100

    parameter comments

--- png2spice/parameters.py
+++ png2spice/parameters.py
@@ -8,21 +8,21 @@ from os.path import join
 
 class CP2SParameters:
     def __init__(self) -> None:
-        self.contrastThreshold          = 170
-        self.imagePadding               = 1200
-        self.cannyThreshold             = 200
-        self.pointDistance              = 850
-        self.imageSliceSize             = 2960   # The size of the subimages of POI analysis
-        self.HLThreshold                = 75     # Hough Lines Transform threshold
-        self.HLMinLineLength            = 940    # Hough Lines Transform minLineLength
+        self.contrastThreshold          = 170   # Preprocessing Contrast
+        self.imagePadding               = 1200  # Image Padding for subset creation
+        self.cannyThreshold             = 200   # Preprocessing for Hough lines
+        self.pointDistance              = 850   # Distance for combining lines
+        self.imageSliceSize             = 2960  # The size of the subimages of POI analysis
+        self.HLThreshold                = 75    # Hough Lines Transform threshold
+        self.HLMinLineLength            = 940   # Hough Lines Transform minLineLength
         self.HLmaxLineGap               = 2     # Hough Lines Transform maxLineGap
         self.HoughIterations            = 10    # Amount of iterations
-        self.HoughThresholdWiggle       = 17
+        self.HoughThresholdWiggle       = 17    # Variationb in iterations
         self.partSnapshotDir            = join(".temp", "output", "snapshots")
-        self.DuplicateVariance          = 2800
-        self.ComponentTerminalAVariance  = 2500
-        self.ComponentTerminalBVariance  = 2400
-        self.minGridStep                = 48
+        self.DuplicateVariance          = 2800  # POI duplicate variance threshold
+        self.ComponentTerminalAVariance  = 2500 # Graphing Terminal A variance threshold
+        self.ComponentTerminalBVariance  = 2400 # Graphing Terminal B variance threshold
+        self.minGridStep                = 48    # Final grid alignment
         self.scalingFactor              = 0
 
     def setScalingFactor(self, scalingFactor) -> float:
